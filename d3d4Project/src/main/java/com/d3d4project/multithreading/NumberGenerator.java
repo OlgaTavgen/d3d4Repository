be4cb0e0f997db9f4479633832e12c6558f21f08
Olga_Tavgen <Olga_Tavgen@EPBYMINW3635.minsk.epam.com>
2018-06-13 19:56:44 +0300

    multithreadingAndConcurrency

---
+++ d3d4Project/src/main/java/com/d3d4project/multithreading/NumberGenerator.java
@@ -0,0 +1,6 @@
+package com.d3d4project.multithreading;
+
+public interface NumberGenerator 
+{
+	int generateNumber(int number);
+}
---
+++ d3d4Project/src/main/java/com/d3d4project/multithreading/SimpleNumberGenerator.java
@@ -0,0 +1,10 @@
+package com.d3d4project.multithreading;
+
+public class SimpleNumberGenerator implements NumberGenerator
+{
+	@Override
+	public int generateNumber(final int number)
+	{
+		return number;
+	}
+}
